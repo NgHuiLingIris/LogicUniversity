67e2ef7e74c0230f27ef1ad3ef0ee476e5dbe1dc
Reshma Radhakrishnan Nair <e0384926@u.nus.edu>
2019-08-10 22:16:15 +0800

    Amendments to charge dept

--- LogicUniversity/Controllers/ChargeBackDepartmentController.cs
+++ LogicUniversity/Controllers/ChargeBackDepartmentController.cs
@@ -9,6 +9,7 @@ using System.Web.Mvc;
 using LogicUniversity.Context;
 using LogicUniversity.Models;
 using PagedList;
+using PagedList.Mvc;
 
 namespace LogicUniversity.Controllers
 {
@@ -25,9 +26,9 @@ namespace LogicUniversity.Controllers
             var Dept_Name = (db.Departments.Select(r => r.DeptName).First()).ToString();
             List<string> months = (System.Globalization.CultureInfo.CurrentCulture.DateTimeFormat.MonthNames).ToList();
             ViewData["months"] = months;
-            ViewData["deptName"] = Dept_Name;
-
-
+          
+           
+        
             return View();
         }
 
@@ -56,44 +57,26 @@ namespace LogicUniversity.Controllers
             }
             if (yearselected == "")
             {
-                year = DateTime.Now.Year.ToString();
-                //year = "1894";
+                year = DateTime.Now.Year.ToString();  
 
             }
+          
 
-
-            List<Products> products = db.Products.ToList();
             List<DisbursementDetail> list = new List<DisbursementDetail>();
             var disbursements = (db.Disbursements.Include(d => d.CollectionPoint).Include(d=>d.Representative).Where(d => d.DateDisbursed.Month == month && d.DateDisbursed.Year.ToString() == year && d.Representative.Department.DeptName == Dept_Name && d.Status == "Delivered")).ToList();
             foreach (Disbursement d in disbursements)
             {
-                List<DisbursementDetail> disbursementlist = (db.DisbursementDetails.Include(dd => dd.StockAdjustmentVoucher).Where(dd => dd.DisbursementId == d.DisbursementId)).ToList();
+                List<DisbursementDetail> disbursementlist = (db.DisbursementDetails.Include(dd => dd.Products).Where(dd => dd.DisbursementId == d.DisbursementId)).ToList();
                 foreach (DisbursementDetail item in disbursementlist)
                 {
                     list.Add(item);
                 }
 
             }
+         
 
-            //Combining disbursementdetails and product details for viewing and class Charge is 
-            //created for this purpose
-            var dlist = (from d in list
-                         join p in products on d.ItemCode equals p.ItemCode
-                         select new Charge
-                         {
-                             disburementDetail = d,
-                             product = p
-                         });
-
-            //returning the combined result
-
-
-            return View(dlist);
+            return View(list);
         }
-
-
-
-
 
 
         protected override void Dispose(bool disposing)
