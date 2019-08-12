cf3d0b0e214406cbf7e05d095beb59f8b46f82aa
Khin Thu Zar Zaw <e0395896@u.nus.edu>
2019-08-12 14:40:47 +0800

    session obj

---
+++ LogicUniversity/Services/Sessions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Collections;
+using System.Web.Mvc;
+
+namespace LogicUniversity.Services
+{
+    public class Sessions
+    {
+        public static Hashtable userSessions = new Hashtable();
+
+    
+    }
+}
